188682d707f159eb599ba53696eb4ea9bad6b2e5
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-10-29 18:52:11 +0900

    Specifying the Data Provider and Connection string setted SQL Server as this Data Provider added the connection string.

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -13,5 +13,19 @@ namespace SamuraiApp.Data
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Battle> Battles { get; set; }
         public DbSet<Quote> Quotes { get; set; }
+
+        /// <summary>
+        /// The following URL shows how to write connection strings
+        /// https://msdn.microsoft.com/en-us/library/system.data.sqlclient.sqlconnection.connectionstring(v=vs.110).aspx
+        /// https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/sql/sqlclient-support-for-localdb
+        /// https://docs.microsoft.com/en-us/sql/database-engine/configure-windows/sql-server-2016-express-localdb
+        /// https://docs.microsoft.com/en-us/sql/odbc/reference/syntax/sqldriverconnect-function
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = @"Server=(LocalDB)\MSSQLLocalDB;Integrated Security=true;Database=SamuraiData;AttachDbFileName=E:\sato\MSSQLLocalDB\SamuraiData.mdf";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
 }
