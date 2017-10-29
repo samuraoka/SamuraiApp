c6f85d13e31343d572f6faad479626981efae9a8
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-10-29 16:26:04 +0900

    added nuget packages for EF Core

--- SamuraiApp.Domain/Samurai.cs
+++ SamuraiApp.Domain/Samurai.cs
@@ -4,6 +4,11 @@ namespace SamuraiApp.Domain
 {
     public class Samurai
     {
+        /// <summary>
+        /// To create a data model, add following NuGet package using following command in Package Manager
+        /// PK> Install-Package Microsoft.EntityFrameworkCore.SqlServer -ProjectName SamuraiApp.Domain -Version 2.0.0
+        /// https://www.nuget.org/packages/Microsoft.EntityFrameworkCore.SqlServer/
+        /// </summary>
         public Samurai()
         {
             Quotes = new List<Quote>();
