e410fbd80360e3d90bf2d43bbea8022e18e29489
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-11-19 03:35:49 +0900

    Entity Framework Core: Getting Started 4. Exploring Mappings and Migrations Creating a Many-to-Many Relationship

---
+++ SamuraiApp.Data/Migrations/20171118183024_JoinTable.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiApp.Data.Migrations
+{
+    public partial class JoinTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Samurais_Battles_BattleId",
+                table: "Samurais");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BattleId",
+                table: "Samurais",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.CreateTable(
+                name: "SamuraiBattle",
+                columns: table => new
+                {
+                    SamuraiId = table.Column<int>(type: "int", nullable: false),
+                    BattleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SamuraiBattle", x => new { x.SamuraiId, x.BattleId });
+                    table.ForeignKey(
+                        name: "FK_SamuraiBattle_Battles_BattleId",
+                        column: x => x.BattleId,
+                        principalTable: "Battles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SamuraiBattle_Samurais_SamuraiId",
+                        column: x => x.SamuraiId,
+                        principalTable: "Samurais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SamuraiBattle_BattleId",
+                table: "SamuraiBattle",
+                column: "BattleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Samurais_Battles_BattleId",
+                table: "Samurais",
+                column: "BattleId",
+                principalTable: "Battles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Samurais_Battles_BattleId",
+                table: "Samurais");
+
+            migrationBuilder.DropTable(
+                name: "SamuraiBattle");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BattleId",
+                table: "Samurais",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Samurais_Battles_BattleId",
+                table: "Samurais",
+                column: "BattleId",
+                principalTable: "Battles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -7,11 +7,14 @@ namespace SamuraiApp.Data
     /// To create a data model, add following NuGet package using the below command in Package Manager
     /// PM> Install-Package Microsoft.EntityFrameworkCore.SqlServer -ProjectName SamuraiApp.Data -Version 2.0.0
     /// https://www.nuget.org/packages/Microsoft.EntityFrameworkCore.SqlServer/
+    /// 
     /// To create migration files, add following NuGet package using the below command in Package Manager
     /// PM> Install-Package Microsoft.EntityFrameworkCore.Tools -ProjectName SamuraiApp.Data -Version 2.0.0
     /// https://www.nuget.org/packages/Microsoft.EntityFrameworkCore.Tools/
+    /// 
     /// To get help messages, use a following command in Package Manager.
     /// PM> get-help entityframeworkcore
+    /// 
     /// To add a new migration, execute a following command
     /// PM> Add-Migration -Name init -Context SamuraiContext -Project SamuraiApp.Data -StartupProject SamuraiApp.Data
     /// And if you have a error, try to add properties to the csproj file.
@@ -19,6 +22,7 @@ namespace SamuraiApp.Data
     /// <GenerateBindingRedirectsOutputType>true</GenerateBindingRedirectsOutputType>
     /// https://stackoverflow.com/questions/45978173/system-valuetuple-version-0-0-0-0-required-for-add-migration-on-net-4-6-1-cla
     /// https://blogs.msdn.microsoft.com/dotnet/2017/08/14/announcing-entity-framework-core-2-0/
+    /// 
     /// To Create or Update database, run following command in the Package Manager Console
     /// PM> Update-Database -Context SamuraiContext -Project SamuraiApp.Data -StartupProject SamuraiApp.Data
     /// </summary>
@@ -40,6 +44,16 @@ namespace SamuraiApp.Data
         {
             var connectionString = @"Server=(LocalDB)\MSSQLLocalDB;Integrated Security=true;Database=SamuraiData;AttachDbFileName=E:\sato\MSSQLLocalDB\SamuraiData.mdf";
             optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        /// <summary>
+        /// Keys (primary)
+        /// https://docs.microsoft.com/en-us/ef/core/modeling/keys
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(sb => new { sb.SamuraiId, sb.BattleId });
         }
     }
 }
