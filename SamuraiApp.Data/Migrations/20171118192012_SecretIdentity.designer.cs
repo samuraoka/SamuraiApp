51ae070269b848965e3895283fa59123a4ebaefc
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-11-19 04:29:31 +0900

    Creating One-to-One Relationship

--- SamuraiApp.Data/Migrations/20171118192012_SecretIdentity.designer.cs
+++ SamuraiApp.Data/Migrations/20171118192012_SecretIdentity.designer.cs
@@ -2,14 +2,16 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 
 namespace SamuraiApp.Data.Migrations
 {
     [DbContext(typeof(SamuraiContext))]
-    partial class SamuraiContextModelSnapshot : ModelSnapshot
+    [Migration("20171118192012_SecretIdentity")]
+    partial class SecretIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
